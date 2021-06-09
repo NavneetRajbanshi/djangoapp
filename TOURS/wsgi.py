73dc7ace02b8843a4a30474214f109eca38cedeb
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-09 08:25:11 +0545

    TOURS Demo

--- TOURS/wsgi.py
+++ TOURS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tasktodo project.
+WSGI config for TOURS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasktodo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TOURS.settings')
 
 application = get_wsgi_application()
