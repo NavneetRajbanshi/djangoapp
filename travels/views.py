83f5d5f475a761aaef05262759f9dcca4470293c
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-22 07:17:31 +0545

    first Django

--- travels/views.py
+++ travels/views.py
@@ -3,16 +3,19 @@ from django.core.checks import messages
 import travels
 from travels.models import Destination
 from django.shortcuts import render
-from .models import Destination, Traveler, Contact
+from .models import Destination, Traveler
 from django.http import HttpResponse
 from .forms import ContactForm
 # Create your views here.
 def index(request):
 
     form = ContactForm()
+    
     if request.method == 'POST':
-        print(request.POST)
-    context = {'form' :form}
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
     dests = Destination.objects.all()
     travelers = Traveler.objects.all()
     
