4fe2ef341f371bf04a70f2a096c24ba306b6bc96
NavneetRajbanshi <navneetrajbanshi85@gmail.com>
2021-06-22 12:57:38 +0545

    first Django

--- travels/views.py
+++ travels/views.py
@@ -6,15 +6,17 @@ from django.shortcuts import render
 from .models import Destination, Traveler
 from django.http import HttpResponse
 from .forms import ContactForm
+from django.views import View
 # Create your views here.
 def index(request):
-
+    
     form = ContactForm()
     
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+        return HttpResponse("thank you for your interaction")
 
     context = {'form': form}
     dests = Destination.objects.all()
@@ -22,3 +24,4 @@ def index(request):
     
 
     return render(request,'travels/index.html', {'dests': dests, 'travelers': travelers, 'context': context})
+
